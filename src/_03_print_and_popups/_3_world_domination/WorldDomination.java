c38ba3fa0c91eb76d37df312000fd364904b7549
league <maddoxseul@sbsd.net>
2025-01-10 18:31:12 -0800

    quiz game in progress

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,15 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String answer = JOptionPane.showInputDialog("do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
-		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		if(answer.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null,"you will rule the world");
+		}
+		// 3. Otherwise, tell them to sign up for classes at The League
+		else {
+			JOptionPane.showMessageDialog(null,"sign up for classes at the league");
+		}
 	}
 }
 
